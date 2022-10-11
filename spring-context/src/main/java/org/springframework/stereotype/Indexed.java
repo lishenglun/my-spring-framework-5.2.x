5cc26b68ad0c63ff8705110a8cb74b4c4e6b707c
lishenglun <wy183055@163.com>
2022-10-12 00:11:30 +0800

    @Indexed

--- spring-context/src/main/java/org/springframework/stereotype/Indexed.java
+++ spring-context/src/main/java/org/springframework/stereotype/Indexed.java
@@ -23,7 +23,20 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Indicate that the annotated element represents a stereotype for the index.
+ * 1、@Indexed：解决@ComponentScan的性能问题，提升检索效率，加快系统启动速度。
+ *
+ * 随着业务模块越来越多，越来越复杂，尤其在比较大的单体架构里面，java文件很多。系统启动的时候，基于我们扫描的路径，扫描加载的东西很多，我们需要去很多目录里面寻找加载java类，
+ * 这里面检索和遍历的效率，随着java文件越来越多，效率就会越来越低，对系统启动的影响越来越大，所以@ComponentScan在启动时存在性能问题
+ *
+ * 我们程序有2个阶段，编译和运行。在编译的时候，我们的代码已经确定了，哪些类中有注解修饰，已经知道了，所以@Indexed就是在编译的时候，帮我们把已经有对应注解修饰的java类都收集起来，存储在一个文件里面去；
+ * 当系统启动的时候，只需要加载这一个索引文件，去这一个文件里面找到要加载的java类的全类路径限定名即可，这样一来就把启动时的性能影响转接到编译时，
+ * 通过一次IO，就找到了要注入容器中的这些Java类的全路径名，把扫描的过程给省略掉了，增加了系统启动速度
+ *
+ * 根据这个索引文件就可以找到所有要载入容器中的Java类。@Component里面内置了@Indexed，所以我们去添加组件注解的话，在spring5.0之后，编译的时候默认都会识别到要被扫描的Java文件，要生效只需要加上spring-context-indexer依赖即可
+ *
+ * <p>
+ *
+ * Indicate that the annotated element represents a stereotype for the index. —— 指示带注解的元素表示索引的构造型。
  *
  * <p>The {@code CandidateComponentsIndex} is an alternative to classpath
  * scanning that uses a metadata file generated at compilation time. The
